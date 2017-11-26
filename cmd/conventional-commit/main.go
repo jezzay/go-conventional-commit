779b0ccff69ba95613d2528a7ee340c74bc9d386
Jeremy <jezzay@users.noreply.github.com>
2017-11-26 17:09:03 +1100

    feat: add header parsing

--- cmd/conventional-commit/main.go
+++ cmd/conventional-commit/main.go
@@ -19,7 +19,7 @@ Commit message body`
 //	// TODO: look at how conventional commit parser parses commit messages for inspiration.
 //
 	message := commit.Parse(commitWithBody)
-	fmt.Printf("Parsed commit message with body = \n%v\n", message)
+	fmt.Printf("Parsed commit message with body = \n%+v\n", message)
 //
 //	message = commit.Parse("feat add new feature")
 //	fmt.Printf("Invalid commit message = %v \n", message)
